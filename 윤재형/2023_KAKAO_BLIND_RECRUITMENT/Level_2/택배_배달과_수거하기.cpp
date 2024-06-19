ce2b23fac53feafa973a6dc667d15ba8bfd2044a
Superdodge <yjh01209@gmail.com>
2024-06-19 17:26:58 +0900

    Update 택배_배달과_수거하기.cpp

--- 윤재형/2023_KAKAO_BLIND_RECRUITMENT/Level_2/택배_배달과_수거하기.cpp
+++ 윤재형/2023_KAKAO_BLIND_RECRUITMENT/Level_2/택배_배달과_수거하기.cpp
@@ -4,52 +4,31 @@
 
 using namespace std;
 
-vector<int> dc = {10, 20, 30, 40};
-vector<int> comb_i(7, 0);
-int emoji_size;
-
-vector<vector<int>> g_users;
-vector<int> g_emoticons;
-vector<int> answer;
-
-void comb(int index){
-    if(index != emoji_size){
-        for(int i = 0; i < 4; ++i){
-            comb_i[index] = i;
-            comb(index + 1);
+void start(int cap, vector<int> &arr, int idx){
+    while(idx >= 0){
+        if(cap >= arr[idx]){
+            cap -= arr[idx];
+            arr[idx--] = 0;
         }
-    }
-    else{
-        int service_user = 0;
-        int emoji_cost = 0;
-        
-        for(int user = 0; user < g_users.size(); ++user){
-            int cost = 0;
-            for(int emoji = 0; emoji < emoji_size; ++emoji)
-                if(g_users[user][0] <= dc[comb_i[emoji]])
-                    cost += g_emoticons[emoji] * (100 - dc[comb_i[emoji]]) / 100;
-            if(cost >= g_users[user][1]) ++service_user;
-            else emoji_cost += cost;
-        }
-        
-        if(answer.size() == 0){
-            answer.push_back(service_user);
-            answer.push_back(emoji_cost);
-        }
-        else if(answer[0] < service_user || (answer[0] == service_user && answer[1] < emoji_cost)){
-            answer[0] = service_user;
-            answer[1] = emoji_cost;
+        else{
+            arr[idx] -= cap;
+            break;
         }
     }
 }
 
-vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
+long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    long long answer = 0;
     
-    g_users = users;
-    g_emoticons = emoticons;
-    emoji_size = emoticons.size();
-    
-    comb(0);
+    for(int i = n - 1; i >= 0;){
+        if(deliveries[i] == 0 && pickups[i] == 0){
+            --i;
+            continue;
+        }
+        start(cap, deliveries, i);
+        start(cap, pickups, i);
+        answer += (i + 1) * 2;
+    }
     
     return answer;
 }
